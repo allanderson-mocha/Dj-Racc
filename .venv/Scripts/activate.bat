9d203cb58f6708acb62fd4fdf9b37dbf88b5c079
Allan Anderson <Allanderson1128@gmail.com>
2023-10-12 13:17:33 -0700

    New Initial Setup for Py11

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Documents\Discord Tokein\DJ Racc Code\.venv
+set VIRTUAL_ENV=C:\Users\mavic\Desktop\Discord Bot\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ main.py
@@ -0,0 +1,9 @@
+import settings
+import discord
+from discord import app_commands
+
+def run():
+    print()
+
+if __name__ == "__main__":
+    run()
