0e8979bed29512934155fbfdc73ccbcd8e71e4f3
Allan Anderson <Allanderson1128@gmail.com>
2023-10-16 12:33:09 -0700

    Implemented connection, sample command, framework for future logging

--- main.py
+++ main.py
@@ -2,8 +2,27 @@ import settings
 import discord
 from discord import app_commands
 
-def run():
-    print()
+logger = settings.logging.getLogger("bot")
 
-if __name__ == "__main__":
-    run()
+intents = discord.Intents(guild_messages = True, reactions = True, message_content = True)
+
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents = intents)
+        self.synced = False
+    
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"{self.user} is in the houseee!")
+
+client = aclient()
+tree = discord.app_commands.CommandTree(client)
+
+@tree.command(description= "Blesses John")
+async def hi_john(interaction: discord.interactions):
+    await interaction.response.send_message('Fuck you, John')
+
+client.run(settings.DISCORD_SECRET_API)
