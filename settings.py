c59b119a1b7541c9879903558c6bd3abbd2f3e4d
Allan Anderson <allanderson1128@gmail.com>
2023-12-16 17:33:44 -0800

    Reinitialization for my code

--- settings.py
+++ settings.py
@@ -1,6 +1,54 @@
 import os
+import logging
 from dotenv import load_dotenv
+from logging.config import dictConfig
 
 load_dotenv()
 
 DISCORD_SECRET_API = os.getenv("DISCORD_API_TOKEN")
+
+logging_config = {
+    "version" : 1,
+    "disabled_existing_Loggers": False,
+    "formatters":{
+        "verbose":{
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(messsage)s"
+        },
+        "standard":{
+            "format": "%(levelname)-10s - %(name)-15s : %(messsage)s"
+        }
+    },
+    "handlers":{
+        "console":{
+            'level': "DEBUG",
+            'class': "logging.StreamHandler",
+            'formatter': "standard"
+        },
+        "console2":{
+            'level': "WARNING",
+            'class': "logging.StreamHandler",
+            'formatter': "standard"
+        },
+        "file":{
+            'level': "INFO",
+            'class': "logging.FileHandler",
+            'filename': "logs/infos.log",
+            'mode': "w"
+        },
+    },
+    "loggers":{
+        "bot":{
+            'handlers': ['console'],
+            "level": "INFO",
+            "propagate": False
+        },
+        "discord":{
+            'handlers': ['console2', "file"],
+            "level": "INFO",
+            "propagate": False
+        }
+    }
+
+}
+
+dictConfig(logging_config)
