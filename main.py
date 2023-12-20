89d4fc740fb31504b5beab73fdc8a8aaa374f177
Allan Anderson <allanderson1128@gmail.com>
2023-12-19 16:51:45 -0800

    Added embed style message response from Youtube search with duration, also temporary options menu for testing

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from googleapiclient.discovery import build
 
 logger = settings.logging.getLogger("bot")
 
-intents = discord.Intents(guild_messages = True, reactions = True, message_content = True)
+intents = discord.Intents.default()
+intents.reactions = True
 
 class aclient(discord.Client):
     def __init__(self):
-        super().__init__(intents = intents)
+        super().__init__(intents=intents)
         self.synced = False
     
+    # Waits for sync to connect bot
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
@@ -23,18 +25,17 @@ class aclient(discord.Client):
 client = aclient()
 tree = discord.app_commands.CommandTree(client)
 
-@tree.command(name = "hi_john", description = "Blesses John")
-async def bye_john(ctx: discord.interactions):
-    await ctx.response.send_message('Fuck you, John')
-
-async def nuh_uh(ctx: discord.interactions):
+# Quick easy message command
+@tree.command(name="salutations", description="Blesses John")
+async def hi_john(ctx: discord.interactions):
     await ctx.response.send_message('Fuck you, John')
 
 # YouTube Search Implementation:
 YOUTUBE_API_KEY = settings.YOUTUBE_API  # Get API from settings
 youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
-@tree.command(name = "youtube_search", description = "Searches Youtube")
+@tree.command(name="youtube_search", description="Searches Youtube")
+
 async def youtube_music_search(ctx: discord.interactions, *, query: str, max_results: int = 5):
     # Calling search.list for search results including snippet and contentDetails
     request = youtube.search().list(
@@ -50,12 +51,53 @@ async def youtube_music_search(ctx: discord.interactions, *, query: str, max_res
         video_id = item['id']['videoId']
         video_title = item['snippet']['title']
         video_url = f'https://www.youtube.com/watch?v={video_id}'
+        video_duration = await get_video_duration(video_id=video_id)
         video_results.append({
             'title' : video_title,
             'url' : video_url,
+            'duration' : video_duration
             })
 
-    formatted_response = '\n'.join([f"Title: {video['title']}\nURL: {video['url']}" for video in video_results])
-    await ctx.response.send_message(video_results)
+    # Temp output
+    embed = discord.Embed(
+        title="Choose the right video:",
+        description="Select from below options",
+        color=discord.Color.blurple()
+    )
+    for video in video_results:
+        embed.add_field(name=f"{video['title']} {video['duration']}", value=video['url'], inline=False)
+    await ctx.response.send_message(embed=embed)
+
+# Function to aqcuire video duration from Youtube API and convert to minutes:seconds format
+async def get_video_duration(video_id):
+    request = youtube.videos().list(
+        part='contentDetails',
+        id = video_id
+    )
+    response = request.execute()
+    try:
+        duration = response['items'][0]['contentDetails']['duration']
+        minutes = int(duration[2:].split('M')[0])
+        seconds = int(duration[-3:-1])
+        duration = f"[{minutes}:{seconds}]"
+        return duration
+    except Exception as e:
+        return None
+
+    
+# Test run for interactive message menus
+@tree.command(name="option_menu", description="experimental menu")
+async def option_menu(ctx: discord.interactions):
+    view = discord.ui.View(timeout=300)
+    options = [
+        discord.ui.Button(style=discord.ButtonStyle.primary, label="Option 1", custom_id="option_1"),
+        discord.ui.Button(style=discord.ButtonStyle.secondary, label="Option 2", custom_id="option_2"),
+        discord.ui.Button(style=discord.ButtonStyle.danger, label="Oh no", custom_id="close_menu")
+    ]
+    for option in options:
+        view.add_item(option)
+    await ctx.response.send_message("choose:", view=view)
+
+
 
 client.run(settings.DISCORD_SECRET_API)
